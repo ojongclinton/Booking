537dc2992c4f52e75acee773ffcde5f41826c216
ojong <82442816+ojongclinton@users.noreply.github.com>
2023-03-08 10:58:51 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes,Route } from "react-router-dom";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 
 function App() {
