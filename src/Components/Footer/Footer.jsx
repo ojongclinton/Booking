cbe3774aeb24bce71d75da5bfe334705ef2d530b
ojong <82442816+ojongclinton@users.noreply.github.com>
2023-04-22 01:07:32 +0100

    Using mui grid in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
   return (
     <div className='footerDivContainer'>
       <Grid container spacing={1} className='footerFirstSection'>
-        <Grid item xl={3} lg={3} md={4} sm={12} xs={12}>
+        <Grid item xl={3} lg={3} md={4} sm={6} xs={12}>
           <div style={{display:'flex',gap:'5px'}}>
             <GiMountaintop size={40}/>
             <h2 style={{marginTop:'5px',color:"#b89146"}}>WEMOVE</h2>
@@ -28,7 +28,7 @@ function Footer() {
             <a href=""><FaLinkedin /></a>
           </div>
         </Grid>
-        <Grid item xl={3} lg={3} md={4} sm={12} xs={12}>
+        <Grid item xl={3} lg={3} md={4} sm={6} xs={12}>
           <h2>Information</h2>
           <div className='infoIconsDiv'>
             <p> <MdLocationPin fill='#b89146' size={25}/> Terminus Odza, Yaounde </p>
@@ -36,14 +36,14 @@ function Footer() {
             <p> <BsFillTelephoneFill fill='#b89146' size={25}/>(+237) 6 52 30 39 43</p>
           </div>
         </Grid>
-        <Grid item xl={3} lg={3} md={4} sm={12} xs={12}>
+        <Grid item xl={3} lg={3} md={4} sm={6} xs={12}>
           <h2>Page Links</h2>
           <p><a href="">Room Cleaning</a></p>
           <p><a href="">Car parking</a></p>
           <p><a href="">Swimming Pool</a></p>
           <p><a href="">Fitness Gym</a></p>
         </Grid>
-        <Grid item xl={3} lg={3} md={4} sm={12} xs={12}>
+        <Grid item xl={3} lg={3} md={4} sm={6} xs={12}>
           <h2>Subscribe</h2>
           <input type="email" placeholder='example@email.com'/>
           <button>SUBSCRIBE</button>
